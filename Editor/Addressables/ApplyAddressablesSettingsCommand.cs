aa969148f1b3a26ef7b04860590a8cc388eaebc1
Mefodei <mefodei.link@gmail.com>
2021-06-30 02:23:03 +0300

    - add addressable settings command

--- Editor/Addressables/ApplyAddressablesSettingsCommand.cs
+++ Editor/Addressables/ApplyAddressablesSettingsCommand.cs
@@ -1,33 +1,68 @@
-using System;
-using UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands;
-using UniModules.UniGame.UniBuild.Editor.ClientBuild.Interfaces;
-using UnityEngine;
+using UniModules.UniCore.EditorTools.Editor.Utility;
+using UnityEditor;
 
-
-[Serializable]
-public class ApplyAddressablesSettingsCommand : SerializableBuildCommand
+namespace UniModules.UniBuild.Commands
 {
-    public override void Execute(IUniBuilderConfiguration buildParameters)
+    using System;
+    using UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands;
+    using UniModules.UniGame.UniBuild.Editor.ClientBuild.Interfaces;
+    using UnityEditor.AddressableAssets;
+    using UnityEditor.AddressableAssets.Settings;
+    using UnityEngine;
+    
+    [Serializable]
+    public class ApplyAddressablesSettingsCommand : SerializableBuildCommand
     {
-        Execute();
-    }
 
 #if ODIN_INSPECTOR
-    [Sirenix.OdinInspector.Button]
+        [Sirenix.OdinInspector.InlineProperty]
+        [Sirenix.OdinInspector.HideLabel]
 #endif
-    public void Execute()
-    {
+        public AddressableBuildSettings settings = new AddressableBuildSettings();
         
+        public override void Execute(IUniBuilderConfiguration buildParameters)
+        {
+            Execute();
+        }
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button]
+#endif
+        public void Execute()
+        {
+            var asset = AddressableAssetSettingsDefaultObject.Settings;
+            if (!asset) return;
+
+            asset.ContiguousBundles = settings.contiguousBundles;
+            asset.BuildRemoteCatalog = settings.buildRemoteCatalog;
+            asset.DisableCatalogUpdateOnStartup = settings.disableCatalogUpdateOnStartUp;
+            asset.buildSettings.LogResourceManagerExceptions = settings.logRuntimeErrors;
+            asset.UniqueBundleIds = settings.uniqueBundleIds;
+            
+            ProjectConfigData.PostProfilerEvents = settings.enableEventProfile;
+
+            asset.MarkDirty();
+            AssetDatabase.Refresh();
+
+        }
+    
     }
-    
-}
 
-[Serializable]
-public class AddressableBuildSettings
-{
+    [Serializable]
+    public class AddressableBuildSettings
+    {
 
-    public bool enableEventProfile;
-    
-    
+        public bool enableEventProfile = false;
 
+        public bool logRuntimeErrors = true;
+
+        public bool disableCatalogUpdateOnStartUp = false;
+
+        public bool buildRemoteCatalog = false;
+
+        public bool uniqueBundleIds = false;
+
+        public bool contiguousBundles = true;
+
+    }
 }
