22a67422e768d63fb1deafb88e4e484fb566e2ba
Mefodei <mefodei.link@gmail.com>
2023-11-25 20:33:20 +0700

    - add open directory command

---
+++ Editor/PathCommands/OpenDirectoryCommand.cs
@@ -0,0 +1,38 @@
+namespace UniModules.UniBuild.Commands.Editor.PathCommands
+{
+    using System;
+    using System.Collections.Generic;
+    using UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands;
+    using UniGame.UniBuild.Editor.ClientBuild.Interfaces;
+    using UnityEditor;
+
+    [Serializable]
+    public class OpenDirectoryCommand : SerializableBuildCommand
+    {
+        public string disableArgument = "-batchmode";
+        
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.FolderPath]
+#endif
+        public List<string> folderPath = new List<string>();
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button]
+#endif
+        public void Execute()
+        {
+            foreach (var folder in folderPath)
+            {
+                EditorUtility.RevealInFinder(folder);
+            }
+        }
+
+        public override void Execute(IUniBuilderConfiguration buildParameters)
+        {
+            if (buildParameters.Arguments.Contains(disableArgument))
+                return;
+            
+            Execute();
+        } 
+    }
+}
