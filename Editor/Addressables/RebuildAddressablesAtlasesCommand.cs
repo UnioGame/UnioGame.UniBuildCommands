65e439d491d1d9c830a74e358b86bc4fd24e0188
Mefodei <mefodei.link@gmail.com>
2024-11-10 01:49:01 +0700

    - update addressable atlases

--- Editor/Addressables/RebuildAddressablesAtlasesCommand.cs
+++ Editor/Addressables/RebuildAddressablesAtlasesCommand.cs
@@ -1,17 +1,29 @@
-﻿using UniModules.UniGame.CoreModules.UniGame.AddressableTools.Editor.AddressableSpriteAtlasManager;
-
-namespace UniModules.UniGame.BuildCommands.Editor.Addressables
+﻿namespace UniModules.UniGame.BuildCommands.Editor.Addressables
 {
     using System;
+    using global::UniGame.AddressableAtlases;
     using global::UniGame.UniBuild.Editor.ClientBuild.Interfaces;
+    using Sirenix.OdinInspector;
     using UniBuild.Editor.ClientBuild.Commands.PreBuildCommands;
+    using UniModules.Editor;
 
     [Serializable]
     public class RebuildAddressablesAtlasesCommand : UnitySerializablePreBuildCommand
     {
         public override void Execute(IUniBuilderConfiguration buildParameters)
         {
-            AddressableSpriteAtlasesEditorHandler.Reimport();
+            Execute();
+        }
+        
+        [Button]
+        public void Execute()
+        {
+            var atlasSettings = AssetEditorTools
+                .GetAssets<AddressableAtlasesSettingsAsset>();
+            foreach (var settings in atlasSettings)
+            {
+                settings.Reimport();
+            }
         }
     }
 }
